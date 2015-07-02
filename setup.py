e4a17c7b5f2dad4bfe01768ff1f72054f2a04055
Ricky Catron <catron.ricky@gmail.com>
2015-07-02 13:32:27 -0400

    Updated version and submited to pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='CarrollPiProject',
-      version='0.2',
+      version='0.2.1',
       description='Functions for interacting with the Pi using the sensors and sending data to the CarrollPiProject website.',
       url='https://github.com/CarrollPiProject/CarrollPiProject',
       author='Carroll Pi Project: NC Division',
