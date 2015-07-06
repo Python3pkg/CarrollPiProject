4364a97e3798acd016c615c4116d1320ef664794
Ricky Catron <catron.ricky@gmail.com>
2015-07-06 09:30:26 -0400

    Changed website to class structure to limit login needs

--- CarrollPiProject/Website.py
+++ CarrollPiProject/Website.py
@@ -1,27 +1,41 @@
 import requests
 
 
-def sendDataToWebsite(celsius, fahrenheit, dissolvedOxygen, school, username, password):
-    """Sends Dissoloved oxygen and Temperature data to the website API: Requires the login token
+class Website:
+    school = ""
+    username = ""
+    password = ""
 
-    Args: \n
-    :param celsius: the temperature in celsius \n
-    :type celsius: str \n
-    :param fahrenheit: the temperature in fahrenheit \n
-    :type fahrenheit: str \n
-    :param dissolvedOxygen: the dissolved oxygen \n
-    :type dissolvedOxygen: str \n
-    :param school: the location of the device \n
-    :type school: str \n
-    :param username: CarrollPiProject Username. \n
-    :type username: str \n
-    :param password: CarrollPiProject Password. \n
-    :type password: str \n
-    """
-    body = {'celsius': celsius,
-            'fahrenheit': fahrenheit,
-            'dissolvedOxygen': dissolvedOxygen,
-            'school': school,
-            'username': username,
-            'password': password}
-    requests.post("http://www.carrollpiproject.com/dataApi", data=body)
+    def __init__(self, school, username, password):
+        """
+        Args: \n
+        :param school: the location of the device \n
+        :type school: str \n
+        :param username: CarrollPiProject Username. \n
+        :type username: str \n
+        :param password: CarrollPiProject Password. \n
+        :type password: str \n
+        """
+
+        self.school = school
+        self.username = username
+        self.password = password
+
+    def sendDataToWebsite(self, celsius, fahrenheit, dissolvedOxygen):
+        """Sends Dissoloved oxygen and Temperature data to the website API
+
+        Args: \n
+        :param celsius: the temperature in celsius \n
+        :type celsius: str \n
+        :param fahrenheit: the temperature in fahrenheit \n
+        :type fahrenheit: str \n
+        :param dissolvedOxygen: the dissolved oxygen \n
+        :type dissolvedOxygen: str \n
+        """
+        body = {'celsius': celsius,
+                'fahrenheit': fahrenheit,
+                'dissolvedOxygen': dissolvedOxygen,
+                'school': self.school,
+                'username': self.username,
+                'password': self.password}
+        requests.post("http://www.carrollpiproject.com/dataApi", data=body)
--- Demos/getResults.py
+++ Demos/getResults.py
@@ -3,15 +3,16 @@
 
 from CarrollPiProject.Temperature import Temperature as temp
 from CarrollPiProject.DissolvedOxygen import dissolved_oxygen
-from CarrollPiProject import Website
+from CarrollPiProject.Website import Website
 
 tempSensor = temp()
 do_sensor = dissolved_oxygen()
+website = Website("North Carroll", "username", "password")
 
 do_sensor.turn_on_lights()
 
 while True:
     do_sensor.pass_temperature(tempSensor.read_temp_celsius())
 
-    Website.sendDataToWebsite(tempSensor.read_temp_celsius(), tempSensor.read_temp_fahrenheit(),
-                              do_sensor.get_data(), "North Carroll", "username", "password")
+    website.sendDataToWebsite(tempSensor.read_temp_celsius(), tempSensor.read_temp_fahrenheit(),
+                              do_sensor.get_data())
